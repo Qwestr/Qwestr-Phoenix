abbf93686d2b862257caa012013c76b6799794d2
Shawn Daichendt <shawn.daichendt@gmail.com>
2015-12-29 15:52:57 -0500

    added login fn to Auth module

--- lib/qwestr/auth.ex
+++ lib/qwestr/auth.ex
@@ -10,4 +10,11 @@ defmodule Qwestr.Auth do
 		user = user_id && repo.get(Qwestr.User, user_id) 
 		assign(conn, :current_user, user)
 	end 
+
+	def login(conn, user) do 
+		conn
+		|> assign(:current_user, user)
+		|> put_session(:user_id, user.id) 
+		|> configure_session(renew: true)
+	end 
 end
