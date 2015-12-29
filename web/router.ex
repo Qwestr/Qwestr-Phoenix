632498ffc57407f43e694199deedabaf65c619dd
Shawn Daichendt <shawn.daichendt@gmail.com>
2015-12-29 13:32:45 -0500

    Added Auth plug to browser pipeline

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Qwestr.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Qwestr.Auth, repo: Qwestr.Repo
   end
 
   pipeline :api do
