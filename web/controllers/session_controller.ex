1a768fd7c0590c69e30a6eeb7d2291af09e13eb5
Shawn Daichendt <shawn.daichendt@gmail.com>
2015-12-29 23:12:03 -0500

    Added delete fn to Session controller

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,12 +1,15 @@
 defmodule Qwestr.SessionController do 
 	use Qwestr.Web, :controller
 
+	alias Qwestr.Auth
+
 	def new(conn, _) do 
 		render conn, "new.html"
 	end 
 
 	def create(conn, %{"session" => %{"username" => user, "password" => pass}}) do
-		case Qwestr.Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
+		
+		case Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
 
 			{:ok, conn} -> 
 				conn
@@ -18,5 +21,12 @@ defmodule Qwestr.SessionController do
 				|> put_flash(:error, "Invalid username/password combination") 
 				|> render("new.html")
 		end 
+	end
+
+	def delete(conn, _) do 
+		conn
+		|> Auth.logout()
+		|> put_flash(:info, "You have been logged out") 
+		|> redirect(to: page_path(conn, :index))
 	end
 end
