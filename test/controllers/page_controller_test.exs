49119308dad5b9c960c7fe85d1b53c402ad1c4dc
Shawn Daichendt <shawn.daichendt@gmail.com>
2015-12-31 18:42:57 -0500

    Updated Page controller tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Qwestr.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Qwestr!"
   end
 end
