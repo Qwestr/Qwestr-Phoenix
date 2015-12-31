f71333e84a96c810797cbeed58f303210b284e03
Shawn Daichendt <shawn.daichendt@gmail.com>
2015-12-31 00:20:17 -0500

    Updated Qwest controller to add user param to actions

--- web/controllers/qwest_controller.ex
+++ web/controllers/qwest_controller.ex
@@ -5,18 +5,29 @@ defmodule Qwestr.QwestController do
 
   plug :scrub_params, "qwest" when action in [:create, :update]
 
-  def index(conn, _params) do
-    qwests = Repo.all(Qwest)
+  def action(conn, _) do 
+    apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_user])
+  end
+
+  def index(conn, _params, user) do
+    qwests = Repo.all(user_qwests(user)) 
     render(conn, "index.html", qwests: qwests)
   end
 
-  def new(conn, _params) do
-    changeset = Qwest.changeset(%Qwest{})
-    render(conn, "new.html", changeset: changeset)
+  def new(conn, _params, user) do 
+    changeset =
+      user
+      |> build(:qwests)
+      |> Qwest.changeset()
+    
+    render(conn, "new.html", changeset: changeset) 
   end
 
-  def create(conn, %{"qwest" => qwest_params}) do
-    changeset = Qwest.changeset(%Qwest{}, qwest_params)
+  def create(conn, %{"qwest" => qwest_params}, user) do 
+    changeset =
+      user
+      |> build(:qwests)
+      |> Qwest.changeset(qwest_params)
 
     case Repo.insert(changeset) do
       {:ok, _qwest} ->
@@ -28,19 +39,19 @@ defmodule Qwestr.QwestController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    qwest = Repo.get!(Qwest, id)
+  def show(conn, %{"id" => id}, user) do 
+    qwest = Repo.get!(user_qwests(user), id) 
     render(conn, "show.html", qwest: qwest)
   end
 
-  def edit(conn, %{"id" => id}) do
-    qwest = Repo.get!(Qwest, id)
+  def edit(conn, %{"id" => id}, user) do
+    qwest = Repo.get!(user_qwests(user), id)
     changeset = Qwest.changeset(qwest)
     render(conn, "edit.html", qwest: qwest, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "qwest" => qwest_params}) do
-    qwest = Repo.get!(Qwest, id)
+  def update(conn, %{"id" => id, "qwest" => qwest_params}, user) do 
+    qwest = Repo.get!(user_qwests(user), id)
     changeset = Qwest.changeset(qwest, qwest_params)
 
     case Repo.update(changeset) do
@@ -53,8 +64,8 @@ defmodule Qwestr.QwestController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    qwest = Repo.get!(Qwest, id)
+  def delete(conn, %{"id" => id}, user) do 
+    qwest = Repo.get!(user_qwests(user), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -63,5 +74,11 @@ defmodule Qwestr.QwestController do
     conn
     |> put_flash(:info, "Qwest deleted successfully.")
     |> redirect(to: qwest_path(conn, :index))
+  end
+
+  # Private Methods
+  
+  defp user_qwests(user) do 
+    assoc(user, :qwests)
   end
 end
