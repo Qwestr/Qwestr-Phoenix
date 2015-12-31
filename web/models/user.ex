6f2a1514f4a9a42bf995585f47276c1d6015a79b
Shawn Daichendt <shawn.daichendt@gmail.com>
2015-12-31 10:53:16 -0500

    Added unique_constraint for username in User model changeset

--- web/models/user.ex
+++ web/models/user.ex
@@ -15,13 +15,15 @@ defmodule Qwestr.User do
 		model
 		|> cast(params, ~w(name username), [])
 		|> validate_length(:username, min: 1, max: 20) 
+		|> unique_constraint(:username)
 	end
 
 	def registration_changeset(model, params) do 
 		model
 		|> changeset(params)
 		|> cast(params, ~w(password), [])
-		|> validate_length(:password, min: 6, max: 100) |> put_pass_hash()
+		|> validate_length(:password, min: 6, max: 100) 
+		|> put_pass_hash()
 	end
 
 	defp put_pass_hash(changeset) do 
