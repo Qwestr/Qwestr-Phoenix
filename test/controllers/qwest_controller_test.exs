4fb50ec2f64b7f012db259e222c4fd56e49e875e
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-01-02 12:27:14 -0500

    Added new Qwest controller tests

--- test/controllers/qwest_controller_test.exs
+++ test/controllers/qwest_controller_test.exs
@@ -1,9 +1,10 @@
 defmodule Qwestr.QwestControllerTest do
   use Qwestr.ConnCase
 
-  # alias Qwestr.Qwest
-  # @valid_attrs %{title: "New Qwest"}
-  # @invalid_attrs %{}
+  alias Qwestr.Qwest
+
+  @valid_attrs %{title: "New Qwest"}
+  @invalid_attrs %{}
 
   setup config do
     if config[:logged_in] do
@@ -33,15 +34,36 @@ defmodule Qwestr.QwestControllerTest do
 
   @tag :logged_in
   test "lists all user's qwests on index", %{conn: conn, user: user} do
-    # setup qwests
     user_qwest = insert_qwest(user, title: "user qwest")
     other_qwest = insert_qwest(insert_user(username: "other"), title: "another qwest")
     conn = get conn, qwest_path(conn, :index)
     
-    # test assertions
     assert html_response(conn, 200) =~ ~r/Listing qwests/ 
     assert String.contains?(conn.resp_body, user_qwest.title) 
 
     refute String.contains?(conn.resp_body, other_qwest.title)
+  end
+
+  @tag :logged_in
+  test "creates user qwest and redirects", %{conn: conn, user: user} do
+    conn = post conn, qwest_path(conn, :create), qwest: @valid_attrs 
+
+    assert redirected_to(conn) == qwest_path(conn, :index)
+    assert Repo.get_by!(Qwest, @valid_attrs).user_id == user.id
+  end
+
+  @tag :logged_in
+  test "does not create qwest and renders errors when invalid", %{conn: conn} do
+    count_before = qwest_count(Qwest)
+    conn = post conn, qwest_path(conn, :create), qwest: @invalid_attrs 
+
+    assert html_response(conn, 200) =~ "check the errors"
+    assert qwest_count(Qwest) == count_before
+  end
+
+  # Private Methods
+
+  defp qwest_count(query) do 
+    Repo.one(from v in query, select: count(v.id))
   end
 end
