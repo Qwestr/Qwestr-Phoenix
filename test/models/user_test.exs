ebcc27c3df40a8801f707b9742e6a1e409c72e9a
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-01-04 09:28:04 -0500

    Updated UserTest module

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule Qwestr.UserTest do
 
 	alias Qwestr.User
 
-	@valid_attrs %{name: "Qwest User", username: "qwestr", password: "secret"} 
+	@valid_attrs %{name: "Qwestr User", username: "qwestr", password: "secret"} 
 	@invalid_attrs %{}
 
 	test "changeset with valid attributes" do
@@ -17,4 +17,24 @@ defmodule Qwestr.UserTest do
 		
 		refute changeset.valid?
 	end 
+
+	test "registration_changeset password must be at least 6 chars long" do 
+		attrs = Map.put(@valid_attrs, :password, "no")
+		changeset = User.registration_changeset(%User{}, attrs)
+  
+  	refute changeset.valid?
+  
+  	assert {:password, {"should be at least %{count} character(s)", count: 6}} in changeset.errors
+	end
+
+	test "registration_changeset with valid attributes hashes password" do 
+		attrs = Map.put(@valid_attrs, :password, "superstrong")
+		changeset = User.registration_changeset(%User{}, attrs)
+		
+		%{password: pass, password_hash: pass_hash} = changeset.changes
+  
+  	assert changeset.valid?
+  	assert pass_hash
+  	assert Comeonin.Bcrypt.checkpw(pass, pass_hash)
+	end
 end
