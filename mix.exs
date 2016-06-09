b77a9592b6c4439870047d2203cfb14a91ffbe8a
djedi-knight <shawn.daichendt@gmail.com>
2016-06-09 12:47:04 -0400

    Updated postgrex dependency

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Qwestr.Mixfile do
     [
       {:phoenix, "~> 1.0.3"},
       {:phoenix_ecto, "~> 1.1"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, ">= 0.11.0"},
       {:phoenix_html, "~> 2.1"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:cowboy, "~> 1.0"},
